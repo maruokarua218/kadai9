3bf346b29ea2b42903803e7c89cfd94241c5f0b1
rua maruoka <rua.sun218@gmail.com>
2022-06-02 22:45:19 +0900

    データベースに投稿保存できる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,12 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(content: params[:post][:content])
+  end
 end
