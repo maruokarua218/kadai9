8c2aeed0ed7b22b45ffb20f055818d3a086221bd
rua maruoka <rua.sun218@gmail.com>
2022-06-03 01:06:33 +0900

    投稿一覧を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
@@ -7,6 +8,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:post][:content])
+    Post.create(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
